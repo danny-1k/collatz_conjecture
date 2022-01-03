edc0d8277bb517972c268ef9180bed312bfb22ce
Daniel Ik <codingeinstein@gmail.com>
2022-01-03 21:09:04 +0100

    command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 
 def collatz(n):
@@ -13,12 +14,21 @@ def generate(n):
         if n == 1:
             nums.append(n)
             num+=1
-            print(f'It took {num} steps to get to 1')
+            print(f'It took {num} step(s) to get to 1')
             break
         else:
             nums.append(n)
             num+=1
     return nums
-for i in range(1,3000):
-    plt.plot(generate(i))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num',default=2)
+parser.add_argument('--log_scale',default=False)
+args = parser.parse_args()
+if bool(args.log_scale):plt.yscale('log')
+plt.plot(generate(int(args.num)))
 plt.show()
+
+# for i in range(1,3000):
+#     plt.plot(generate(i))
+# plt.show()
